82e07eb869ca800317035c716b4e9f68de1dfafa
Travis McLane <tmclane@gmail.com>
2022-08-24 21:31:52 -0500

    add additional packages

--- arch/setup.sh
+++ arch/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES=(emacs gnupg go)
+PACKAGES=(docker emacs gnupg go pkgconf openssh)
 
 #Install Packages
 sudo pacman -S "${PACKAGES}"
@@ -8,3 +8,7 @@ sudo pacman -S "${PACKAGES}"
 # Install Go programs
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/tools/cmd/goimports@latest
+
+# Activate OpenSSH Server
+sudo systemctl enable sshd.service
+sudo systemctl start sshd.service
